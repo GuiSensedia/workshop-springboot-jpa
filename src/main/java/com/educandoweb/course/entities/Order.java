6d42802dfd5401a58cf5ca6ee1379963cb2ef31c
GuiSensedia <guilherme.santos@sensedia.com>
2023-02-09 10:15:37 -0300

    Implementando o OrderStatus(status do pedido)

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -26,16 +27,20 @@ public class Order implements Serializable {
 	@JsonFormat(shape= JsonFormat.Shape.STRING, pattern= "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 	
 	public Order() {
 	}
-	public Order(Long id, Instant moment, User client) {
-		this.client = client;
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
 	}
 	
 	public User getClient() {
@@ -57,6 +62,15 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+	
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+		}
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
