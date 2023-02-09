893e4ab0d0b56754e0c3252eebe4ae7cb0178b6a
GuiSensedia <guilherme.santos@sensedia.com>
2023-02-09 15:08:33 -0300

    Finalizando a implementação da entidade OrderItem

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,11 +2,12 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,29 +15,29 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	
 	private Long id;
 	@JsonFormat(shape= JsonFormat.Shape.STRING, pattern= "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
 	private Integer orderStatus;
-	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {
 	}
-	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
-		
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {	
 		this.id = id;
 		this.moment = moment;
 		setOrderStatus(orderStatus);
@@ -48,6 +49,9 @@ public class Order implements Serializable {
 	}
 	public void setClient(User client) {
 		this.client = client;
+	}
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 	public Long getId() {
 		return id;
