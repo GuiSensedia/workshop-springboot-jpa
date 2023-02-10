6c9f336203bbc05699a594f9a3e00225c2e8c4e4
GuiSensedia <guilherme.santos@sensedia.com>
2023-02-10 11:48:48 -0300

    Instanciando Classe Payment

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.educandoweb.course.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,24 +17,33 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
+
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	@JsonFormat(shape= JsonFormat.Shape.STRING, pattern= "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
 	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -44,11 +54,18 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 	
+	
 	public User getClient() {
 		return client;
 	}
 	public void setClient(User client) {
 		this.client = client;
+	}
+	public Payment getPyament () {
+		return payment;
+	}
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 	public Set<OrderItem> getItems(){
 		return items;
@@ -75,6 +92,8 @@ public class Order implements Serializable {
 		this.orderStatus = orderStatus.getCode();
 		}
 	}
+	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
