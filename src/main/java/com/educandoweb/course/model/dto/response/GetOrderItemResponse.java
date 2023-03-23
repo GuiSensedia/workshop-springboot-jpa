d6c716563eafb93950323adf417534fdc469fca6
GuiSensedia <guilherme.santos@sensedia.com>
2023-03-23 15:51:16 -0300

    change response of orderStatus Integer to OrderStatus Enum

--- src/main/java/com/educandoweb/course/model/dto/response/GetOrderItemResponse.java
+++ src/main/java/com/educandoweb/course/model/dto/response/GetOrderItemResponse.java
@@ -12,16 +12,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class GetOrderItemResponse {
 
+    private String nameProduct;
     private Integer quantity;
     private Double price;
     private Double subTotal;
 
     public static GetOrderItemResponse valueOf(OrderItemDomain orderItem){
         GetOrderItemResponse getOrderItemResponse = GetOrderItemResponse.builder()
+                .nameProduct(orderItem.getId().getProduct().getName())
                 .quantity(orderItem.getQuantity())
                 .price(orderItem.getPrice())
                 .subTotal(orderItem.getSubTotal())
                 .build();
         return getOrderItemResponse;
     }
+
 }
--- src/main/java/com/educandoweb/course/model/dto/response/GetOrderResponse.java
+++ src/main/java/com/educandoweb/course/model/dto/response/GetOrderResponse.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.model.dto.response;
 
+import com.educandoweb.course.enums.OrderStatus;
 import com.educandoweb.course.model.domain.OrderDomain;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
@@ -21,14 +22,14 @@ public class GetOrderResponse {
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern= "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
-    private Integer orderStatus;
+    private OrderStatus orderStatus;
 
     private List<GetOrderItemResponse> orderItem = new ArrayList<>();
 
     public static GetOrderResponse valueOf(OrderDomain orderDomain){
         GetOrderResponse orderResponse = GetOrderResponse.builder()
                 .moment(orderDomain.getMoment())
-                .orderStatus(orderDomain.getOrderStatus())
+                .orderStatus(OrderStatus.valueOf(orderDomain.getOrderStatus()))
                 .orderItem(orderDomain.getItems()
                         .stream()
                         .map(GetOrderItemResponse::valueOf).collect(Collectors.toList()))
