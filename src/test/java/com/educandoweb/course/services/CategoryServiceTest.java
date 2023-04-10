47753d735c6595bb6221f3588284fad98555410f
GuiSensedia <guilherme.santos@sensedia.com>
2023-04-10 15:25:27 -0300

    Create test for GetAll and Create Category.

--- src/test/java/com/educandoweb/course/services/CategoryServiceTest.java
+++ src/test/java/com/educandoweb/course/services/CategoryServiceTest.java
@@ -1,19 +1,20 @@
 package com.educandoweb.course.services;
 
 import com.educandoweb.course.model.domain.CategoryDomain;
+import com.educandoweb.course.model.dto.request.CreateCategoryRequest;
 import com.educandoweb.course.repositories.CategoryRepository;
-import org.glassfish.jaxb.core.v2.TODO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -24,36 +25,62 @@ class CategoryServiceTest {
     @InjectMocks
     private CategoryService categoryService;
 
+    @InjectMocks
+    private CreateCategoryRequest createCategoryRequest;
+
     @Mock
     private CategoryRepository categoryRepository;
 
+    //Given -> Dados de entrada do teste, dados mockados, repositórios mockados.
+    //When  -> Chamada ao endpoint em questão que sendo testado.
+    //Then  -> Verificar se todos os repositories foram testados, verificar se o retorno da chamada está válido.
+    //TODO Create Delete and Update test;
+
     @Test
     void shouldGetCategoryById() {
-        //TODO - Jogar cada ação em um método e chamá-los dentro do teste getCategoryById().
-        //Given - Dados de entrada do teste, dados mockados, repositórios mockados.
         givenGetCategoryById();
-
-        //When - Chamada ao endpoint em questão que sendo testado.
         whenGetCategoryByIdCalled();
-
-        //Then - Verificar se todos os repositories foram testados, verificar se o retorno da chamada está válido.
-        thenGetCategoryByIdCalledOnce();
+        thenExpectedGetCategoryByIdCalledOnce();
     }
 
-    void shouldGetAllCategories(){}
+    @Test
+    void shouldGetAllCategories(){
+        givenGetAllCategories();
+        whenGetAllCategoriesCalled();
+        thenExpectedGetAllCategoriesCalledOnce();
+    }
 
-    void shouldCreateCategory(){}
+    @Test
+    void shouldCreateCategory(){
+        givenCreateCategory();
+        whenCreateCategoryCalled();
+        thenExpectedCreateCategoryCalledOnce();
+    }
 
+    @Test
     void shouldDeleteCategory(){}
 
+    @Test
     void shouldUpdateCategory(){}
 
     /*
      * Given methods
      */
     private void givenGetCategoryById() {
-        Optional<CategoryDomain> optionalCategory = Optional.of(new CategoryDomain(2L, "Melquisedec"));
+        Optional<CategoryDomain> optionalCategory = Optional.of(new CategoryDomain(2L, "Console"));
         when(categoryRepository.findById(anyLong())).thenReturn(optionalCategory);
+    }
+
+    private void givenGetAllCategories() {
+        List<CategoryDomain> listCategory = new ArrayList<>();
+        listCategory.add(new CategoryDomain(1L, "Software"));
+        listCategory.add(new CategoryDomain(2L, "Hardware"));
+        when(categoryRepository.findAll()).thenReturn(listCategory);
+    }
+
+    private void givenCreateCategory() {
+        CategoryDomain domain = new CategoryDomain(5L, "Decoration");
+        when(categoryRepository.save(domain));
     }
 
     /*
@@ -63,11 +90,27 @@ class CategoryServiceTest {
         categoryService.getCategoryById(anyLong());
     }
 
+    private void whenGetAllCategoriesCalled() {
+        categoryService.getAllCategories();
+    }
+
+    private void whenCreateCategoryCalled() {
+        categoryService.createCategory(createCategoryRequest);
+    }
+
     /*
      * Then methods
      */
-    private void thenGetCategoryByIdCalledOnce() {
+    private void thenExpectedGetCategoryByIdCalledOnce() {
         verify(categoryRepository, times(1)).findById(anyLong());
+    }
+
+    private void thenExpectedGetAllCategoriesCalledOnce() {
+        verify(categoryRepository, times(1)).findAll();
+    }
+
+    private void thenExpectedCreateCategoryCalledOnce() {
+        verify(categoryRepository, times(1)).save(any(CategoryDomain.class));
     }
 
 }
